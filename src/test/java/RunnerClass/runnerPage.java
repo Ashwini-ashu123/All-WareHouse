1e86eac117666ce3fa9a2368d2c25103aeb8cbe0
Ashwini Deepak <ashwini.ashu123@gmail.com>
2025-11-21 16:59:55 +0530

    new commit

--- src/test/java/RunnerClass/runnerPage.java
+++ src/test/java/RunnerClass/runnerPage.java
@@ -1,6 +1,5 @@
 package RunnerClass;
 
-
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -8,12 +7,14 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/Featurefile",
-        glue = {"StepDefnFile","hooksClass"},
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
-        tags = "@SmokeTesting or @Bug"
+        glue = {"StepDefnFile", "hooksClass"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber.json",
+                "junit:target/cucumber.xml"   
+        }
 )
-
 public class runnerPage {
 
 }
-
